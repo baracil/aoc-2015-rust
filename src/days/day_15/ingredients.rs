96cb6a4034f0916c75ce9acf442f4435694ff59e
Bastien Aracil <findbastien@gmail.com>
2021-12-16 00:34:47 +0100

    solve day 15

---
+++ src/days/day_15/ingredients.rs
@@ -0,0 +1,77 @@
+use std::str::FromStr;
+
+pub struct Ingredient {
+    capacity:i32,
+    durability:i32,
+    flavor:i32,
+    texture:i32,
+    calories:i32
+}
+
+#[derive(Default)]
+pub struct Recipe {
+    nb_ingredients:i32,
+    capacity:i32,
+    durability:i32,
+    flavor:i32,
+    texture:i32,
+    calories:i32
+}
+
+impl Recipe {
+    pub fn nb_ingredients_left(&self) -> i32 {
+        100-self.nb_ingredients
+    }
+
+    pub fn has_right_amount_of_calories(&self) -> bool {
+        self.calories == 500
+    }
+}
+
+
+impl Recipe {
+    pub(crate) fn score(&self) -> u32 {
+        (self.capacity.max(0)
+        *self.durability.max(0)
+        *self.texture.max(0)
+        *self.flavor.max(0)) as u32
+    }
+}
+
+impl Recipe {
+
+    pub fn add_ingredient(&mut self,ingredient:&Ingredient, quantity:i32) {
+        self.nb_ingredients+=quantity;
+        self.capacity += ingredient.capacity*quantity;
+        self.durability += ingredient.durability*quantity;
+        self.flavor += ingredient.flavor*quantity;
+        self.texture += ingredient.texture*quantity;
+        self.calories += ingredient.calories*quantity;
+    }
+
+}
+
+
+impl FromStr for Ingredient {
+    type Err = String;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let tokens:Vec<&str> = line.split(' ').collect();
+        let capacity = parse_i32(tokens[2]);
+        let durability = parse_i32(tokens[4]);
+        let flavor = parse_i32(tokens[6]);
+        let texture = parse_i32(tokens[8]);
+        let calories = parse_i32(tokens[10]);
+
+        Ok(Ingredient {  capacity, durability, flavor, texture, calories })
+    }
+
+}
+
+fn parse_i32(token:&str) -> i32 {
+    if token.ends_with(',') {
+        return (token)[0..token.len()-1].parse::<i32>().unwrap();
+    }
+    token.parse::<i32>().unwrap()
+
+}
---
+++ src/days/day_15/main.rs
@@ -0,0 +1,46 @@
+use crate::days::day_15::ingredients::Ingredient;
+use crate::{parse_input, Part};
+use crate::days::day_15::solver::{solve, solve_with_calories};
+use crate::problem::{AOCResult, Problem};
+
+#[allow(dead_code)]
+pub fn day15_launch(part: Part) -> AOCResult<String> {
+    let ingredients = parse_input(false)?;
+    match part {
+        Part::Part1 => part1(&ingredients),
+        Part::Part2 => part2(&ingredients)
+    }
+}
+
+fn part1(ingredients:&[Ingredient]) -> AOCResult<String> {
+    Ok(solve(ingredients).to_string())
+}
+
+fn part2(ingredients:&[Ingredient]) -> AOCResult<String> {
+    Ok(solve_with_calories(ingredients).to_string())
+}
+
+#[allow(dead_code)]
+fn parse_input(for_test:bool) -> AOCResult<Vec<Ingredient>> {
+    Problem::factory(for_test)(15).read_input_as_mapped_lines(|l| parse_input!(l,Ingredient))
+}
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    use crate::days::day_15::main::{parse_input, part1, part2};
+
+    #[test]
+    fn day15_part1_test()  {
+        let _input = parse_input(true).unwrap();
+        let result = part1(&_input).unwrap();
+        assert_eq!(result,"62842880")
+    }
+
+    #[test]
+    fn day15_part2_test()  {
+        let _input = parse_input(true).unwrap();
+        let result = part2(&_input).unwrap();
+        assert_eq!(result,"57600000")
+    }
+}
